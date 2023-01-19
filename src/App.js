01dbc71ad2fbe50fc27c0bbdede30a8c026d4075
seanwhill <seanhill2016@gmail.com>
2023-01-18 19:27:14 -0500

    Make the image / mouse enter computation generic to scale

--- src/App.js
+++ src/App.js
@@ -1,193 +1,114 @@
 import logo from './logo.svg';
 import './App.css';
-import { memo, useMemo, useRef, useState } from 'react';
+import { createRef, memo, useEffect, useMemo, useRef, useState } from 'react';
 import x from './gran-2.svg'
 
+const ExpensiveCard = ({ onRef, selected, imgVal, id }) => {
+  const ref = useRef(null)
+
+  useEffect(() => {
+    console.log("setting ref")
+    onRef(ref, id)
+  }, [])
+
+
+  // setRefs(prevRefs => [...prevRefs, ref]);
+  const handleImageLoad = (id, ref) => {
+    const canvas = ref.current;
+    const ctx = canvas.getContext('2d');
+
+    const img = document.getElementById(id);
+    canvas.width = img.width
+    canvas.height = img.height
+    ctx.drawImage(img, 0, 0);
+
+    // const imgData = ctx.getImageData(0, 0, img.width, img.height);
+    img.style.display = "none"
+  }
+
+
+  return (
+    <>
+      <img
+        onLoad={() => handleImageLoad(id, ref)}
+        src={imgVal} id={id} alt="logo" usemap="#workmap" />
+      <canvas
+        ref={ref}
+        className={`${id} ${selected === id ? "open" : ""}`} />
+    </>
+
+  )
+}
+
 function App() {
-  const [selected, setSelected] = useState(false)
-  const canvasRef = useRef(null);
+  const [selected, setSelected] = useState("")
 
-  const [selected2, setSelected2] = useState(false)
-  const canvasRef2 = useRef(null);
-
-  const refs = useRef([])
-
-  // // Check if the pixel is transparent (alpha value is 0)
-  // if (alpha === 0) {
-  //   console.log("Mouse is over a transparent pixel");
-  //   setSelected(false)
-
-  // } else {
-  //   setSelected(true)
-
-  //   console.log("Mouse is over a non-transparent pixel");
-  // }
-  // selected2(false)
+  const [refs, setRefs] = useState([])
 
 
-  const handleMouseMove = (event) => {
-    const canvas = canvasRef.current;
+  //TODO only run every 20 or so pixels. Expensive computation
+  const handleMouseMoveNew = (event) => {
+    for (const ref of refs) {
+      const rect = ref[0].current.getBoundingClientRect();
 
-    const image1 = document.getElementById("test1");
-    const image2 = document.getElementById("test2");
+      if (event.clientX >= rect.left && event.clientX <= rect.right && event.clientY >= rect.top && event.clientY <= rect.bottom) {
+        // console.log("Mouse entered image");
 
-    const rect1 = canvasRef.current.getBoundingClientRect();
-    // const rect1 = image1.getBoundingClientRect();
-    const rect2 = canvasRef2.current.getBoundingClientRect();
+        const canvas = ref[0].current;
+        const ctx = canvas.getContext('2d');
 
-    // console.log(rect1)
-    // console.log(rect2)
+        // Get the coordinates of the mouse relative to the canvas
+        const x = event.clientX - canvas.offsetLeft;
+        const y = event.clientY - canvas.offsetTop;
 
-    if (event.clientX >= rect1.left && event.clientX <= rect1.right && event.clientY >= rect1.top && event.clientY <= rect1.bottom) {
-      console.log("Mouse entered image 1");
-
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-
-      // Get the coordinates of the mouse relative to the canvas
-      const x = event.clientX - canvas.offsetLeft;
-      const y = event.clientY - canvas.offsetTop;
-
-      console.log(x)
-      console.log(y)
-
-      // Get the image data for the pixel at the mouse's coordinates
-      // const imgData = ctx.getImageData(x, y, 1, 1);
-
-      // console.log(imgData)
-      // // The alpha value is stored in the fourth element (index 3) of the data array
-      // const alpha = imgData.data[3];
-
-      // console.log(alpha)
-
-
-      var pixels = ctx.getImageData(x - 7.5, y - 7.5, 15, 15).data;
-      for (var i = 0; i < pixels.length; i += 4) {
-        var alpha = pixels[i + 3];
-        if (alpha > 0) {
-          console.log("Mouse is within 10 pixels of non-transparent section");
-          setSelected(true)
-          break;
-        } else {
-          setSelected(false) //TODO improve
+        var pixels = ctx.getImageData(x - 7.5, y - 7.5, 15, 15).data;
+        for (var i = 0; i < pixels.length; i += 4) {
+          var alpha = pixels[i + 3];
+          if (alpha > 0) {
+            // console.log("Mouse is within 10 pixels of non-transparent section");
+            setSelected(ref[1])
+            return
+          } else {
+            // console.log('Not within 10 pxiels')
+            setSelected("") //TODO improve
+          }
         }
+
       }
-
-    } else if (event.clientX >= rect2.left && event.clientX <= rect2.right && event.clientY >= rect2.top && event.clientY <= rect2.bottom) {
-      console.log("Mouse entered image 2");
-
-      const canvas = canvasRef2.current;
-      const ctx = canvas.getContext('2d');
-
-      // Get the coordinates of the mouse relative to the canvas
-      const x = event.clientX - canvas.offsetLeft;
-      const y = event.clientY - canvas.offsetTop;
-
-      console.log(x)
-      console.log(y)
-
-      // Get the image data for the pixel at the mouse's coordinates
-      const imgData = ctx.getImageData(x, y, 1, 1);
-
-      console.log(imgData)
-      // The alpha value is stored in the fourth element (index 3) of the data array
-      const alpha = imgData.data[3];
-
-      console.log(alpha)
-      // Check if the pixel is transparent (alpha value is 0)
-      if (alpha === 0) {
-        console.log("Mouse is over a transparent pixel");
-        setSelected2(false)
-
-      } else {
-        setSelected2(true)
-        console.log("Mouse is over a non-transparent pixel");
+      else {
+        setSelected("")
       }
-      setSelected(false)
     }
-    else {
-      setSelected(false)
-      setSelected2(false)
-    }
-
-
   };
 
-  //TODO image load is only rendering some of the image
-  const handleImageLoad = () => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
 
-    const img = document.getElementById('test1');
-    canvas.width = img.width
-    canvas.height = img.height
-    ctx.drawImage(img, 0, 0);
+  useEffect(() => {
+    console.log("new ref added")
+    console.log(refs)
+  }, [refs])
 
-    console.log(img.width)
-    console.log(img.height)
-    const imgData = ctx.getImageData(0, 0, img.width, img.height);
-    img.style.display = "none"
-    console.log(imgData);
+
+  function handleRef(ref, id) {
+    setRefs(prevRefs => [...prevRefs, [ref, id]]);
   }
 
-  const handleImageLoad2 = () => {
-    const canvas = canvasRef2.current;
-    const ctx = canvas.getContext('2d');
-
-    const img = document.getElementById('test2');
-    canvas.width = img.width
-    canvas.height = img.height
-    ctx.drawImage(img, 0, 0);
-
-    console.log(img.width)
-    console.log(img.height)
-    const imgData = ctx.getImageData(0, 0, img.width, img.height);
-    img.style.display = "none"
-    console.log(imgData);
-  }
-
-  const ExpensiveCard = memo(() => {
-
-
-    const id = "gran_lift"
-    return (
-      <>
-        <img
-          onLoad={handleImageLoad}
-          src={require('./x_0000_Gran-Lift.png')} id={"test1"} className={`gran ${selected ? "open" : ""}`} alt="logo" usemap="#workmap" />
-        <canvas
-          ref={canvasRef}
-          className={`gran ${selected ? "open" : ""}`} />
-      </>
-
-    )
-  })
-
-  const ExpensiveCard2 = memo(() => {
-
-    return (
-      <>
-        <img
-          onLoad={handleImageLoad2}
-          src={require('./south_Sjourn.png')} id="test2" className={`sjourn ${false ? "open" : ""}`} alt="logo" usemap="#workmap" />
-        <canvas
-          ref={canvasRef2}
-          className={`sjourn ${selected2 ? "open" : ""}`} />
-      </>
-
-    )
-  })
 
   return (
     <div className="App">
       <header className="App-header">
-        <div className='container'
-          onMouseMove={handleMouseMove}
-        >
-          <img src={require('./img_0034_Background.jpg')} className="image" alt="logo" usemap="#workmap" />
+        <div>
+          <button style={{ zIndex: 1000 }} onClick={() => { setSelected(true); console.log("clicked") }}>TESSSSSSSSSSSSSSSSSSSST</button>
 
-          <ExpensiveCard />
-          <ExpensiveCard2 />
+        </div>
+
+        <div className='container'
+          onMouseMove={handleMouseMoveNew}
+        >
+
+          <img src={require('./img_0034_Background.jpg')} className="image" alt="logo" usemap="#workmap" />
+          <ExpensiveCard key="gran" id="gran_lift" onRef={handleRef} selected={selected} imgVal={require('./x_0000_Gran-Lift.png')} />
+          <ExpensiveCard key="sjourn" id="sjourn" onRef={handleRef} selected={selected} imgVal={require('./south_Sjourn.png')} />
+
 
 
 
